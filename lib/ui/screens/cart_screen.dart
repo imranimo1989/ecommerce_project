a3d3c40a153b0f88216f5c8b0cbe2e25c98005cb
Mohammed Imran <emon08ctg@gmail.com>
2023-05-16 15:19:56 +0600

    wishlist ui done

--- lib/ui/screens/cart_screen.dart
+++ lib/ui/screens/cart_screen.dart
@@ -1,10 +1,20 @@
+import 'package:ecommerce_project/ui/state_manager/bottom_navigation_bar_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Cart Screen"),),);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Get.find<BottomNavigationBarController>().backToHome();
+          }, icon: const Icon(Icons.arrow_back)
+        ),title: const Text("Cart"),
+      ),
+    );
   }
 }
--- lib/ui/screens/wishlist_screen.dart
+++ lib/ui/screens/wishlist_screen.dart
@@ -1,10 +1,36 @@
+import 'package:ecommerce_project/ui/state_manager/bottom_navigation_bar_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../widgets/product_card_widget.dart';
 
 class WishlistScreen extends StatelessWidget {
   const WishlistScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Wishlist"),),);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Get.find<BottomNavigationBarController>().backToHome();
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: const Text("My Wish List"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, childAspectRatio: .82,mainAxisSpacing: 4,crossAxisSpacing: 4
+
+
+        ),
+            itemCount: 10,
+            itemBuilder: (context, index){
+              return const ProductCardWidget();
+            }),
+      ),
+    );
   }
 }
